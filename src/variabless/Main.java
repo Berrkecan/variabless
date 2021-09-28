72e4994df5be8aa3f2b7b403aba9b016b40d0575
Berrkecan <berrkecantosun@gmail.com>
2021-09-28 17:06:00 +0300

    Initial commit

---
+++ src/variabless/Main.java
@@ -0,0 +1,17 @@
+package variabless;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String phoneName="Oppo Realme 9 Pro 128 GB";
+		double phonePrice=3886.48;
+		double phoneStar=4.7;
+		boolean phoneIsDiscound=true;
+		int phoneDiscound=7;
+		System.out.println("FÝYAT :" +phonePrice* (100-phoneDiscound)/100);
+		
+
+	}
+
+}
